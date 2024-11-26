f3975b9e956a730e5c3cfbfc2f9628edd6b590ec
Janis Danisevskis <jdanisevskis@aurora.tech>
2024-11-25 16:51:57 -0800

    make linter happy

--- test/asn1.cpp
+++ test/asn1.cpp
@@ -14,11 +14,10 @@
  * limitations under the License.
  */
 
+#include <gtest/gtest.h>
 #include <nat20/asn1.h>
 
-#include <gtest/gtest.h>
-
-class IntegerTest: public testing::TestWithParam<std::tuple<int64_t, std::vector<uint8_t>>> {};
+class IntegerTest : public testing::TestWithParam<std::tuple<int64_t, std::vector<uint8_t>>> {};
 
 std::vector<uint8_t> ENCODED_0 = {0x02, 0x01, 0x0};
 std::vector<uint8_t> ENCODED_127 = {0x02, 0x01, 0x7f};
@@ -27,14 +26,14 @@ std::vector<uint8_t> ENCODED_256 = {0x02, 0x02, 0x01, 0x00};
 std::vector<uint8_t> ENCODED_MINUS_128 = {0x02, 0x01, 0x80};
 std::vector<uint8_t> ENCODED_MINUS_129 = {0x02, 0x02, 0xff, 0x7f};
 
-INSTANTIATE_TEST_CASE_P(Asn1IntegerTest, IntegerTest, testing::Values(
-    std::tuple(0, ENCODED_0),
-    std::tuple(127, ENCODED_127),
-    std::tuple(128, ENCODED_128),
-    std::tuple(256, ENCODED_256),
-    std::tuple(-128, ENCODED_MINUS_128),
-    std::tuple(-129, ENCODED_MINUS_129)
-));
+INSTANTIATE_TEST_CASE_P(Asn1IntegerTest,
+                        IntegerTest,
+                        testing::Values(std::tuple(0, ENCODED_0),
+                                        std::tuple(127, ENCODED_127),
+                                        std::tuple(128, ENCODED_128),
+                                        std::tuple(256, ENCODED_256),
+                                        std::tuple(-128, ENCODED_MINUS_128),
+                                        std::tuple(-129, ENCODED_MINUS_129)));
 
 TEST_P(IntegerTest, IntegerEncoding) {
     auto [i, expected] = GetParam();
@@ -45,12 +44,12 @@ TEST_P(IntegerTest, IntegerEncoding) {
     n20_asn1_int64(&s, i);
     ASSERT_TRUE(n20_asn1_stream_is_good(&s));
     ASSERT_EQ(n20_asn1_stream_data_written(&s), expected.size());
-    std::vector<uint8_t> got = std::vector<uint8_t>(n20_asn1_stream_data(&s), n20_asn1_stream_data(&s) + n20_asn1_stream_data_written(&s));
+    std::vector<uint8_t> got = std::vector<uint8_t>(
+        n20_asn1_stream_data(&s), n20_asn1_stream_data(&s) + n20_asn1_stream_data_written(&s));
     ASSERT_EQ(expected, got);
-
 }
 
-class BitStringTest: public testing::TestWithParam<std::tuple<size_t, std::vector<uint8_t>>> {};
+class BitStringTest : public testing::TestWithParam<std::tuple<size_t, std::vector<uint8_t>>> {};
 
 std::vector<uint8_t> PROTO_BITS = {0x6e, 0x5d, 0xc5};
 
@@ -60,13 +59,13 @@ std::vector<uint8_t> ENCODED_BITS_18 = {0x03, 0x04, 0x06, 0x6e, 0x5d, 0xc0};
 std::vector<uint8_t> ENCODED_BITS_23 = {0x03, 0x04, 0x01, 0x6e, 0x5d, 0xc4};
 std::vector<uint8_t> ENCODED_BITS_24 = {0x03, 0x04, 0x00, 0x6e, 0x5d, 0xc5};
 
-INSTANTIATE_TEST_CASE_P(Asn1BitStringTest, BitStringTest, testing::Values(
-    std::tuple(16, ENCODED_BITS_16),
-    std::tuple(17, ENCODED_BITS_17),
-    std::tuple(18, ENCODED_BITS_18),
-    std::tuple(23, ENCODED_BITS_23),
-    std::tuple(24, ENCODED_BITS_24)
-));
+INSTANTIATE_TEST_CASE_P(Asn1BitStringTest,
+                        BitStringTest,
+                        testing::Values(std::tuple(16, ENCODED_BITS_16),
+                                        std::tuple(17, ENCODED_BITS_17),
+                                        std::tuple(18, ENCODED_BITS_18),
+                                        std::tuple(23, ENCODED_BITS_23),
+                                        std::tuple(24, ENCODED_BITS_24)));
 
 TEST_P(BitStringTest, BitStringEncoding) {
     auto [i, expected] = GetParam();
@@ -77,6 +76,7 @@ TEST_P(BitStringTest, BitStringEncoding) {
     n20_asn1_bitstring(&s, PROTO_BITS.data(), i);
     ASSERT_TRUE(n20_asn1_stream_is_good(&s));
     ASSERT_EQ(n20_asn1_stream_data_written(&s), expected.size());
-    std::vector<uint8_t> got = std::vector<uint8_t>(n20_asn1_stream_data(&s), n20_asn1_stream_data(&s) + n20_asn1_stream_data_written(&s));
+    std::vector<uint8_t> got = std::vector<uint8_t>(
+        n20_asn1_stream_data(&s), n20_asn1_stream_data(&s) + n20_asn1_stream_data_written(&s));
     ASSERT_EQ(expected, got);
 }
