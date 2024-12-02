a28428f14e2d33ed9bb33663a2b71c645e3ddafd
Andy Young <anyoung@aurora.tech>
2024-12-02 13:06:07 -0800

    Declare uint8_t vectors as const

--- test/asn1.cpp
+++ test/asn1.cpp
@@ -21,8 +21,8 @@
 
 class BooleanTest : public testing::TestWithParam<std::tuple<bool, std::vector<uint8_t>>> {};
 
-std::vector<uint8_t> ENCODED_FALSE = {0x01, 0x01, 0x00};
-std::vector<uint8_t> ENCODED_TRUE = {0x01, 0x01, 0xff};
+std::vector<uint8_t> const ENCODED_FALSE = {0x01, 0x01, 0x00};
+std::vector<uint8_t> const ENCODED_TRUE = {0x01, 0x01, 0xff};
 
 INSTANTIATE_TEST_CASE_P(Asn1BooleanTest,
                         BooleanTest,
@@ -110,15 +110,15 @@ TEST_P(BitStringTest, BitStringEncoding) {
 class OctetStringTest
     : public testing::TestWithParam<std::tuple<std::vector<uint8_t>, std::vector<uint8_t>>> {};
 
-std::vector<uint8_t> BYTES_ZERO = {};
-std::vector<uint8_t> BYTES_ONE = {0x03};
-std::vector<uint8_t> BYTES_TWO = {0x02, 0x01};
-std::vector<uint8_t> BYTES_THREE = {0xff, 0x00, 0xa1};
+std::vector<uint8_t> const BYTES_ZERO = {};
+std::vector<uint8_t> const BYTES_ONE = {0x03};
+std::vector<uint8_t> const BYTES_TWO = {0x02, 0x01};
+std::vector<uint8_t> const BYTES_THREE = {0xff, 0x00, 0xa1};
 
-std::vector<uint8_t> ENCODED_BYTES_ZERO = {0x04, 0x0};
-std::vector<uint8_t> ENCODED_BYTES_ONE = {0x04, 0x01, 0x03};
-std::vector<uint8_t> ENCODED_BYTES_TWO = {0x04, 0x02, 0x02, 0x01};
-std::vector<uint8_t> ENCODED_BYTES_THREE = {0x04, 0x03, 0xff, 0x00, 0xa1};
+std::vector<uint8_t> const ENCODED_BYTES_ZERO = {0x04, 0x0};
+std::vector<uint8_t> const ENCODED_BYTES_ONE = {0x04, 0x01, 0x03};
+std::vector<uint8_t> const ENCODED_BYTES_TWO = {0x04, 0x02, 0x02, 0x01};
+std::vector<uint8_t> const ENCODED_BYTES_THREE = {0x04, 0x03, 0xff, 0x00, 0xa1};
 
 INSTANTIATE_TEST_CASE_P(Asn1OctetStringTest,
                         OctetStringTest,
@@ -143,7 +143,7 @@ TEST_P(OctetStringTest, OctetStringEncoding) {
 
 class NullTest : public testing::Test {};
 
-std::vector<uint8_t> ENCODED_NULL = {0x05, 0x00};
+std::vector<uint8_t> const ENCODED_NULL = {0x05, 0x00};
 
 TEST(NullTest, NullEncoding) {
     n20_asn1_stream_t s;
@@ -160,9 +160,9 @@ TEST(NullTest, NullEncoding) {
 class PrintableStringTest
     : public testing::TestWithParam<std::tuple<std::string, std::vector<uint8_t>>> {};
 
-std::vector<uint8_t> ENCODED_STRING_EMPTY = {0x13, 0x00};
-std::vector<uint8_t> ENCODED_STRING_NOT_EMPTY = {0x13, 0x01, 0x7e};
-std::vector<uint8_t> ENCODED_STRING_FULL_CHARSET = {
+std::vector<uint8_t> const ENCODED_STRING_EMPTY = {0x13, 0x00};
+std::vector<uint8_t> const ENCODED_STRING_NOT_EMPTY = {0x13, 0x01, 0x7e};
+std::vector<uint8_t> const ENCODED_STRING_FULL_CHARSET = {
     0x13, 0x4a, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e,
     0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x61, 0x62, 0x63, 0x64,
     0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74,
@@ -195,7 +195,7 @@ TEST_P(PrintableStringTest, OctetStringEncoding) {
 class GeneralizedTimeTest
     : public testing::TestWithParam<std::tuple<std::string, std::vector<uint8_t>>> {};
 
-std::vector<uint8_t> ENCODED_TIME_ZERO = {0x18,
+std::vector<uint8_t> const ENCODED_TIME_ZERO = {0x18,
                                           0x0f,
                                           0x30,
                                           0x30,
@@ -212,7 +212,7 @@ std::vector<uint8_t> ENCODED_TIME_ZERO = {0x18,
                                           0x30,
                                           0x30,
                                           0x5a};
-std::vector<uint8_t> ENCODED_TIME_NOT_ZERO = {0x18,
+std::vector<uint8_t> const ENCODED_TIME_NOT_ZERO = {0x18,
                                               0x0f,
                                               0x32,
                                               0x30,
