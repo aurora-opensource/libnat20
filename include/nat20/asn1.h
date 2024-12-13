c43e9b844ed69948c6fb79d6e438a2c81f570cd1
Janis Danisevskis <jdanisevskis@aurora.tech>
2024-12-13 08:46:33 -0800

    Gracefully hande OIDs with too high element count. (#22)
    If the element_count field of n20_asn1_object_identifier_t is
    larger than N20_ASN1_MAX_OID_ELEMENTS it leads to out
    of bounds access in n20_asn1_object_identifier.
    
    With patch an ASN.1 NULL is rendered if a malformed object
    identifier is encountered.
    
    Also fixup struct naming convention on oid and stream.

--- include/nat20/asn1.h
+++ include/nat20/asn1.h
@@ -230,7 +230,7 @@ typedef uint8_t n20_asn1_class_t;
  * each field is known when rendering the corresponding header
  * with no further adjustment required to adhere to DER.
  */
-typedef struct n20_asn1_stream {
+typedef struct n20_asn1_stream_s {
     /**
      * @brief Points to the beginning of the underlying buffer.
      *
@@ -502,7 +502,11 @@ extern void n20_asn1_null(n20_asn1_stream_t *const s);
 /**
  * @brief Write an object identifier complete with ASN.1 header to the given stream.
  *
- * If the `oid` parameter is NULL this function behaves like
+ * If the @ref oid parameter is NULL this function behaves like
+ * @ref n20_asn1_null.
+ *
+ * If `oid->element_count` is initialized to avalue greater than
+ * @ref N20_ASN1_MAX_OID_ELEMENTS this function behaves like
  * @ref n20_asn1_null.
  *
  * @param s The stream that is to be updated.
