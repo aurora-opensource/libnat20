a92380c1a7ae79568371e78fef9b2c5f97c6256e
Russell Seidel <rseidel@aurora.tech>
2024-12-16 17:30:28 -0500

    add explicit tags.

--- src/x509_ext_open_dice_input.c
+++ src/x509_ext_open_dice_input.c
@@ -30,39 +30,83 @@ void n20_x509_ext_open_dice_input_content(n20_asn1_stream_t *const s, void *cont
         return;
     }
 
-    size_t mark = n20_asn1_stream_data_written(s);
+    size_t sequence_mark = n20_asn1_stream_data_written(s);
+    size_t mark = sequence_mark;
 
     // profileName [7] EXPLICIT UTF8String OPTIONAL
     // Don't include this if it's NULL.
     if (open_dice_input->profile_name != NULL) {
         n20_asn1_printablestring(s, open_dice_input->profile_name);
+        n20_asn1_header(s,
+                        N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                        /*constructed=*/true,
+                        /*tag=*/7,
+                        n20_asn1_stream_data_written(s) - mark);
+        mark = n20_asn1_stream_data_written(s);
     }
 
+    // Mode ::= INTEGER (0..3)
     // mode [6] EXPLICIT Mode OPTIONAL
     n20_asn1_uint64(s, inputs->mode);
+    n20_asn1_header(s,
+                    N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                    /*constructed=*/true,
+                    /*tag=*/6,
+                    n20_asn1_stream_data_written(s) - mark);
+    mark = n20_asn1_stream_data_written(s);
 
     // authorityDescriptor [5] EXPLICIT OCTET STRING OPTIONAL
     // Don't include this if it's NULL.
     if (inputs->authority_descriptor != NULL) {
         n20_asn1_octetstring(s, inputs->authority_descriptor, inputs->authority_descriptor_length);
+        n20_asn1_header(s,
+                        N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                        /*constructed=*/true,
+                        /*tag=*/5,
+                        n20_asn1_stream_data_written(s) - mark);
+        mark = n20_asn1_stream_data_written(s);
     }
 
     // authorityHash [4] EXPLICIT OCTET STRING OPTIONAL
     n20_asn1_octetstring(s, inputs->authority_hash, sizeof(inputs->authority_hash));
+    n20_asn1_header(s,
+                    N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                    /*constructed=*/true,
+                    /*tag=*/4,
+                    n20_asn1_stream_data_written(s) - mark);
+    mark = n20_asn1_stream_data_written(s);
 
     switch (inputs->configuration_format) {
         case n20_open_dice_configuration_format_inline_e:
             // configurationDescriptor [3] EXPLICIT OCTET STRING OPTIONAL
             n20_asn1_octetstring(
                 s, inputs->configuration_inline, sizeof(inputs->configuration_inline));
+            n20_asn1_header(s,
+                            N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                            /*constructed=*/true,
+                            /*tag=*/3,
+                            n20_asn1_stream_data_written(s) - mark);
+            mark = n20_asn1_stream_data_written(s);
             break;
         case n20_open_dice_configuration_format_descriptor_e:
             // configurationDescriptor [3] EXPLICIT OCTET STRING OPTIONAL
             n20_asn1_octetstring(
                 s, inputs->configuration_descriptor, inputs->configuration_descriptor_length);
+            n20_asn1_header(s,
+                            N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                            /*constructed=*/true,
+                            /*tag=*/3,
+                            n20_asn1_stream_data_written(s) - mark);
+            mark = n20_asn1_stream_data_written(s);
 
             // configurationHash [2] EXPLICIT OCTET STRING OPTIONAL
             n20_asn1_octetstring(s, inputs->configuration_hash, sizeof(inputs->configuration_hash));
+            n20_asn1_header(s,
+                            N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                            /*constructed=*/true,
+                            /*tag=*/2,
+                            n20_asn1_stream_data_written(s) - mark);
+            mark = n20_asn1_stream_data_written(s);
             break;
     }
 
@@ -70,14 +114,26 @@ void n20_x509_ext_open_dice_input_content(n20_asn1_stream_t *const s, void *cont
     // Don't include this if it's NULL.
     if (inputs->code_descriptor != NULL) {
         n20_asn1_octetstring(s, inputs->code_descriptor, inputs->code_descriptor_length);
+        n20_asn1_header(s,
+                        N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                        /*constructed=*/true,
+                        /*tag=*/1,
+                        n20_asn1_stream_data_written(s) - mark);
+        mark = n20_asn1_stream_data_written(s);
     }
 
     // codeHash [0] EXPLICIT OCTET STRING OPTIONAL
     n20_asn1_octetstring(s, inputs->code_hash, sizeof(inputs->code_hash));
+    n20_asn1_header(s,
+                    N20_ASN1_CLASS_CONTEXT_SPECIFIC,
+                    /*constructed=*/true,
+                    /*tag=*/0,
+                    n20_asn1_stream_data_written(s) - mark);
+    mark = n20_asn1_stream_data_written(s);
 
     n20_asn1_header(s,
                     N20_ASN1_CLASS_UNIVERSAL,
                     /*constructed=*/true,
                     N20_ASN1_TAG_SEQUENCE,
-                    n20_asn1_stream_data_written(s) - mark);
+                    n20_asn1_stream_data_written(s) - sequence_mark);
 }
