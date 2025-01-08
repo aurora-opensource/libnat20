8a83f330e86d6ab9aa8d663600a3f6c63565d94d
Russell Seidel <rseidel@aurora.tech>
2025-01-08 14:44:59 -0500

    implement TCG DICE TCB Info and Multi Info X509 extensions.

---
+++ include/nat20/x509_ext_tcg_dice_tcb_info.h
@@ -0,0 +1,320 @@
+/*
+ * Copyright 2024 Aurora Operations, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file */
+
+#pragma once
+
+#include <nat20/asn1.h>
+#include <nat20/oid.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup n20_x509_ext_tcg_dice_tcb_info_operational_flags_macros TCG DICE TCB Info Operational
+ * Flags Macros.
+ *
+ * These macros set the corresponding flags in the operational flags (and mask)
+ * bit mask of @ref n20_x509_ext_tcg_dice_tcb_info_operational_flags_t.
+ * Each macro takes a pointer to an instance of
+ * @ref n20_x509_ext_tcg_dice_tcb_info_operational_flags_t as its argument.
+ *
+ * See TCG DICE Attestation Architecture Version 1.1, Section 6.1.1.1.
+ *
+ * # Example
+ *
+ * @code{.c}
+ * n20_x509_ext_tcg_dice_tcb_info_operational_flags_t operational_flags = {0};
+ * N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_SECURE(&operational_flags);
+ * @endcode
+ * @{
+ */
+
+/**
+ * @brief The target environment is not configured for normal operation.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_CONFIGURED(operational_flags) \
+    (operational_flags)->operational_flags_mask[0] |= 0x80
+/**
+ * @brief The target environment is not secure.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_SECURE(operational_flags) \
+    (operational_flags)->operational_flags_mask[0] |= 0x40
+/**
+ * @brief The target environment is recovering (e.g. from a failure).
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_RECOVERY(operational_flags) \
+    (operational_flags)->operational_flags_mask[0] |= 0x20
+/**
+ * @brief The target environment can be debuged.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_DEBUG(operational_flags) \
+    (operational_flags)->operational_flags_mask[0] |= 0x10
+/**
+ * @brief The target environment is vulnerable to replay attack.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_REPLAY_PROTECTED( \
+    operational_flags)                                                             \
+    (operational_flags)->operational_flags_mask[0] |= 0x08
+/**
+ * @brief The target environment is vulneerable to modification by unauthorized updates.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_INTEGRITY_PROTECTED( \
+    operational_flags)                                                                \
+    (operational_flags)->operational_flags_mask[0] |= 0x04
+/**
+ * @brief The target environment is not measured after being loaded into memory.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_RUNTIME_MEASURED( \
+    operational_flags)                                                             \
+    (operational_flags)->operational_flags_mask[0] |= 0x02
+/**
+ * @brief The target environment is mutable.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_IMMUTABLE(operational_flags) \
+    (operational_flags)->operational_flags_mask[0] |= 0x01
+/**
+ * @brief – The Target Environment measurements are not measurements of a Trusted Computing Base
+ * (TCB).
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_TCB(operational_flags) \
+    (operational_flags)->operational_flags_mask[1] |= 0x80
+/**
+ * @brief Used to force a fixed width of for the OperationalFlags (and OperationalFlagsMask) bit
+ * string.
+ */
+#define N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_FIXED_WIDTH(operational_flags) \
+    (operational_flags)->operational_flags_mask[3] |= 0x01
+/**
+ * @}
+ */
+
+/**
+ * @brief TCG DICE TCB Info Operational Flags.
+ *
+ * This type is used to populate the TCB Info Operational Flags and Operational Flags Mask
+ * ASN.1 bitstrings.
+ *
+ * Always zero initialize instances of this structure and
+ * set the flags using the @ref n20_x509_ext_tcg_dice_tcb_info_operational_flags_macros.
+ *
+ * # Example
+ *
+ * @code{.c}
+ * n20_x509_ext_tcg_dice_tcb_info_operational_flags_t operational_flags = {0};
+ * N20_X509_EXT_TCG_DICE_TCB_INFO_OPERATIONAL_FLAGS_SET_NOT_SECURE(&operational_flags);
+ * @endcode
+ *
+ * (See TCG DICE Attestation Architecture Version 1.1, Section 6.1.1.1.)
+ */
+typedef struct n20_x509_ext_tcg_dice_tcb_info_operational_flags_s {
+    /**
+     * @brief The operational flags mask.
+     *
+     * Use the @ref n20_x509_ext_tcg_dice_tcb_info_operational_flags_macros to set the individual
+     * bits of the operation flags mask. As described in
+     * @ref n20_x509_ext_tcg_dice_tcb_info_operational_flags_t, there is no need to access this
+     * field directly.
+     */
+    uint8_t operational_flags_mask[4];
+} n20_x509_ext_tcg_dice_tcb_info_operational_flags_t;
+
+/**
+ * @brief TCG DICE TCB FW ID.
+ *
+ * (See TCG DICE Attestation Architecture Version 1.1, Section 6.1.1.)
+ */
+typedef struct n20_x509_ext_tcg_dice_tcb_info_fwid_s {
+    /**
+     * @brief Hash algorithm OID of the algorithm used to generate the digest.
+     */
+    n20_asn1_object_identifier_t hash_algo;
+    /**
+     * @brief Digest of firmware, initialization values, or other settings of the Target
+     * Environment.
+     *
+     * If the digest buffer is NULL, no digest is added to the extension.
+     */
+    n20_asn1_slice_t digest;
+} n20_x509_ext_tcg_dice_tcb_info_fwid_t;
+
+/**
+ * @brief List of TCG DICE FWIDs.
+ *
+ * If the list is NULL, no FWIDs are inserted into the TCG DICE TCB Info extension.
+ */
+typedef struct n20_x509_ext_tcg_dice_tcb_info_fwid_list_s {
+    /**
+     * @brief List of FWIDs to include in the TCG DICE TCB Info extension.
+     */
+    n20_x509_ext_tcg_dice_tcb_info_fwid_t const *list;
+    /**
+     * @brief Number of FWIDs in the list.
+     */
+    size_t count;
+} n20_x509_ext_tcg_dice_tcb_info_fwid_list_t;
+
+/**
+ * @brief TCG DICE TCB Info X509 extension context.
+ *
+ * This is the context expected by
+ * @ref n20_x509_ext_tcg_dice_tcb_info_content.
+ * An instance of this object must be passed to the callback.
+ * This is typically done using @ref n20_x509_extension by
+ * initializing @ref n20_x509_extension_t.content_cb with
+ * @ref n20_x509_ext_tcg_dice_tcb_info_content and setting
+ * @ref n20_x509_extension_t.context to an instance of this
+ * struct.
+ *
+ * (See TCG DICE Attestation Architecture Version 1.1, Section 6.1.1.)
+ * @sa OID_TCG_DICE_TCB_INFO
+ */
+typedef struct n20_x509_ext_tcg_dice_tcb_info_s {
+    /**
+     * @brief The entity that created the measurement of the Target Environment.
+     *
+     * If NULL, vendor is not included in the generated extension.
+     */
+    char const *vendor;
+    /**
+     * @brief The product name associated with the measurement of the Target Environment.
+     *
+     * If NULL, model is not included in the generated extension.
+     */
+    char const *model;
+    /**
+     * @brief The revision string associated with the Target Environment.
+     *
+     * If NULL, version is not included in the generated extension.
+     */
+    char const *version;
+    /**
+     * @brief The security version number associated with the Target Environment.
+     */
+    int64_t svn;
+    /**
+     * @brief The DICE layer associated with this measurement of the Target Environment.
+     */
+    int64_t layer;
+    /**
+     * @brief A value that distinguishes different instances of the same type of Target Environment.
+     */
+    int64_t index;
+    /**
+     * @brief List of FWIDs to include in the extension.
+     *
+     * @sa n20_x509_ext_tcg_dice_tcb_info_fwid_list_t
+     */
+    n20_x509_ext_tcg_dice_tcb_info_fwid_list_t fwids;
+    /**
+     * @brief A list of flags that enumerate potentially simultaneous operational states of the
+     * Target Environment.
+     *
+     * @sa n20_x509_ext_tcg_dice_tcb_info_operational_flags_t
+     */
+    n20_x509_ext_tcg_dice_tcb_info_operational_flags_t flags;
+    /**
+     * @brief Used to commumicate which bits in @ref flags are meaningful.
+     *
+     * When a bit is set here, the bit at the corresponding position in @ref flags is meaninful to
+     * the verifier.
+     *
+     * @sa n20_x509_ext_tcg_dice_tcb_info_operational_flags_t
+     */
+    n20_x509_ext_tcg_dice_tcb_info_operational_flags_t flags_mask;
+    /**
+     * @brief Vendor supplied values that encode vendor, model, or device specific state.
+     *
+     * If vender_info.buffer is NULL vendor info is not included in the generated extension.
+     */
+    n20_asn1_slice_t vendor_info;
+    /**
+     * @brief A machine readable description of the measurement.
+     *
+     * If type.buffer is NULL, type is not included in the generated extension.
+     */
+    n20_asn1_slice_t type;
+} n20_x509_ext_tcg_dice_tcb_info_t;
+
+/**
+ * @brief Renders the value of a TCG DICE TCB Info X509 extension.
+ *
+ * The function expects a pointer to an instance of
+ * @ref n20_x509_ext_tcg_dice_tcb_info_t as @ref context argument.
+ *
+ * If @ref context is NULL, nothing is rendered, which would leave
+ * the resulting TCG DICE TCB Info extension malformed.
+ *
+ * This function is typically not used directly but instead
+ * passed to @ref n20_x509_extension by initializing an
+ * instance of @ref n20_x509_extensions_t
+ * (See @ref n20_x509_extension for an example).
+ */
+extern void n20_x509_ext_tcg_dice_tcb_info_content(n20_asn1_stream_t *const s, void *context);
+
+/**
+ * @brief TCG DICE Multi TCB Info X509 extension context.
+ *
+ * This is the context expected by
+ * @ref n20_x509_ext_tcg_dice_multi_tcb_info_content.
+ * An instance of this object must be passed to the callback.
+ * This is typically done using @ref n20_x509_extension by
+ * initializing @ref n20_x509_extension_t.content_cb with
+ * @ref n20_x509_ext_tcg_dice_multi_tcb_info_content and setting
+ * @ref n20_x509_extension_t.context to an instance of this
+ * struct.
+ *
+ * (See TCG DICE Attestation Architecture Version 1.1, Section 6.1.2.)
+ * @sa OID_TCG_DICE_MULTI_TCB_INFO
+ */
+typedef struct n20_x509_ext_tcg_dice_multi_tcb_info_s {
+    /**
+     * @brief List of TCB Info to include in the extension.
+     *
+     * If NULL, no list is rendered in the extension.
+     *
+     * @sa n20_x509_ext_tcg_dice_tcb_info_t
+     */
+    n20_x509_ext_tcg_dice_tcb_info_t const *list;
+    /**
+     * @brief Number of elements in the list.
+     */
+    size_t count;
+} n20_x509_ext_tcg_dice_multi_tcb_info_t;
+
+/**
+ * @brief Renders the value of a TCG DICE Multi TCB Info X509 extension.
+ *
+ * The function expects a pointer to an instance of
+ * @ref n20_x509_ext_tcg_dice_multi_tcb_info_t as @ref context argument.
+ *
+ * If @ref context is NULL, or the list is NULL or number of elements in the list is 0, nothing is
+ * rendered, which would leave the resulting TCG DICE Multi TCB Info extension malformed.
+ *
+ * This function is typically not used directly but instead
+ * passed to @ref n20_x509_extension by initializing an
+ * instance of @ref n20_x509_extensions_t
+ * (See @ref n20_x509_extension for an example).
+ */
+extern void n20_x509_ext_tcg_dice_multi_tcb_info_content(n20_asn1_stream_t *const s, void *context);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/core/x509_ext_tcg_dice_tcb_info.c
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2024 Aurora Operations, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <nat20/asn1.h>
+#include <nat20/x509.h>
+#include <nat20/x509_ext_tcg_dice_tcb_info.h>
+
+static void n20_x509_tcg_dice_tcb_info_fwid_content(n20_asn1_stream_t *const s, void *context) {
+    n20_x509_ext_tcg_dice_tcb_info_fwid_t const *const fwid =
+        (n20_x509_ext_tcg_dice_tcb_info_fwid_t const *)context;
+
+    // fwid is never NULL since it's always called from n20_x509_tcg_dice_tcb_info_fwid_list_content
+    // with a valid pointer.
+
+    // digest OCTET STRING
+    n20_asn1_octetstring(s, &fwid->digest, n20_asn1_tag_info_no_override());
+
+    // hashAlg OBJECT IDENTIFIER
+    n20_asn1_object_identifier(s, &fwid->hash_algo, n20_asn1_tag_info_no_override());
+}
+
+static void n20_x509_tcg_dice_tcb_info_fwid_list_content(n20_asn1_stream_t *const s,
+                                                         void *context) {
+    n20_x509_ext_tcg_dice_tcb_info_fwid_list_t const *const fwid_list =
+        (n20_x509_ext_tcg_dice_tcb_info_fwid_list_t const *)context;
+
+    // fwid_list is never NULL since it's always called from n20_x509_ext_tcg_dice_tcbinfo_content
+    // with a valid pointer.
+    // Additionally, fwid_list->fwids isn't NULL since it was checked in
+    // n20_x509_ext_tcg_dice_tcbinfo_content.
+    for (size_t count = fwid_list->count; count != 0; --count) {
+        n20_asn1_sequence(s,
+                          n20_x509_tcg_dice_tcb_info_fwid_content,
+                          (void *)&fwid_list->list[count - 1],
+                          n20_asn1_tag_info_no_override());
+    }
+}
+
+static void n20_x509_ext_tcg_dice_tcb_info_sequence_content(n20_asn1_stream_t *const s,
+                                                            void *context) {
+    n20_x509_ext_tcg_dice_tcb_info_t const *tcg_dice_tcb_info = context;
+
+    // tcg_dice_tcb_info is never null since it's checked by n20_x509_ext_tcg_dice_tcb_info_content.
+
+    // flagsMask [10] IMPLICIT OperationalFlagsMask OPTIONAL
+    n20_asn1_bitstring(s,
+                       tcg_dice_tcb_info->flags_mask.operational_flags_mask,
+                       8 * sizeof(tcg_dice_tcb_info->flags_mask.operational_flags_mask),
+                       n20_asn1_tag_info_implicit(10));
+
+    // type [9] IMPLICIT OCTET STRING OPTIONAL
+    if (NULL != tcg_dice_tcb_info->type.buffer) {
+        n20_asn1_octetstring(s, &tcg_dice_tcb_info->type, n20_asn1_tag_info_implicit(9));
+    }
+
+    // vendorInfo [8] IMPLICIT OCTET STRING OPTIONAL
+    if (NULL != tcg_dice_tcb_info->vendor_info.buffer) {
+        n20_asn1_octetstring(s, &tcg_dice_tcb_info->vendor_info, n20_asn1_tag_info_implicit(8));
+    }
+
+    // flags [7] IMPLICIT OperationalFlags OPTIONAL
+    n20_asn1_bitstring(s,
+                       tcg_dice_tcb_info->flags.operational_flags_mask,
+                       8 * sizeof(tcg_dice_tcb_info->flags.operational_flags_mask),
+                       n20_asn1_tag_info_implicit(7));
+
+    // fwids [6] IMPLICIT FWIDLIST OPTIONAL
+    if (NULL != tcg_dice_tcb_info->fwids.list) {
+        n20_asn1_sequence(s,
+                          n20_x509_tcg_dice_tcb_info_fwid_list_content,
+                          (void *)&tcg_dice_tcb_info->fwids,
+                          n20_asn1_tag_info_implicit(6));
+    }
+
+    // index [5] IMPLICIT INTEGER OPTIONAL
+    n20_asn1_int64(s, tcg_dice_tcb_info->index, n20_asn1_tag_info_implicit(5));
+
+    // layer [4] IMPLICIT INTEGER OPTIONAL
+    n20_asn1_int64(s, tcg_dice_tcb_info->layer, n20_asn1_tag_info_implicit(4));
+
+    // svn [3] IMPLICIT INTEGER OPTIONAL
+    n20_asn1_int64(s, tcg_dice_tcb_info->svn, n20_asn1_tag_info_implicit(3));
+
+    // version [2] IMPLICIT UTF8String OPTIONAL
+    // TODO(rseidel): Change this to UTF-8 string after it's implemented.
+    if (NULL != tcg_dice_tcb_info->version) {
+        n20_asn1_printablestring(s, tcg_dice_tcb_info->version, n20_asn1_tag_info_implicit(2));
+    }
+
+    // model [1] IMPLICIT UTF8String OPTIONAL
+    // TODO(rseidel): Change this to UTF-8 string after it's implemented.
+    if (NULL != tcg_dice_tcb_info->model) {
+        n20_asn1_printablestring(s, tcg_dice_tcb_info->model, n20_asn1_tag_info_implicit(1));
+    }
+
+    // vendor [0] IMPLICIT UTF8String OPTIONAL
+    // TODO(rseidel): Change this to UTF-8 string after it's implemented.
+    if (NULL != tcg_dice_tcb_info->vendor) {
+        n20_asn1_printablestring(s, tcg_dice_tcb_info->vendor, n20_asn1_tag_info_implicit(0));
+    }
+}
+
+void n20_x509_ext_tcg_dice_tcb_info_content(n20_asn1_stream_t *const s, void *context) {
+    if (NULL == context) {
+        return;
+    }
+
+    n20_asn1_sequence(s,
+                      n20_x509_ext_tcg_dice_tcb_info_sequence_content,
+                      context,
+                      n20_asn1_tag_info_no_override());
+}
+
+static void n20_x509_ext_tcg_dice_multi_tcb_info_sequence_content(n20_asn1_stream_t *const s,
+                                                                  void *context) {
+    n20_x509_ext_tcg_dice_multi_tcb_info_t const *const tcg_dice_multi_tcb_info = context;
+
+    // tcg_dice_multi_tcb_info is never NULL since it's checked by
+    // n20_x509_ext_tcg_dice_multi_tcb_info_content.
+    if (NULL == tcg_dice_multi_tcb_info->list) {
+        return;
+    }
+
+    for (size_t count = tcg_dice_multi_tcb_info->count; count != 0; --count) {
+        n20_x509_ext_tcg_dice_tcb_info_content(s,
+                                               (void *)&tcg_dice_multi_tcb_info->list[count - 1]);
+    }
+}
+
+void n20_x509_ext_tcg_dice_multi_tcb_info_content(n20_asn1_stream_t *const s, void *context) {
+    if (NULL == context) {
+        return;
+    }
+
+    n20_asn1_sequence(s,
+                      n20_x509_ext_tcg_dice_multi_tcb_info_sequence_content,
+                      context,
+                      n20_asn1_tag_info_no_override());
+}
