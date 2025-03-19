063c15e08eb1c618d05fc86f27dbb110489f7934
Janis Danisevskis <jdanisevskis@aurora.tech>
2025-03-19 14:23:10 -0700

    [CBOR] Add CBOR rendering primitives.
    Add code for rendering CBOR artefacts.
    Add unit tests for CBOR encoder.

---
+++ src/core/cbor.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2025 Aurora Operations, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <nat20/cbor.h>
+#include <nat20/stream.h>
+#include <nat20/types.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void n20_cbor_write_header(n20_stream_t *const s, n20_cbor_type_t cbor_type, uint64_t n) {
+    uint8_t header = (uint8_t)(cbor_type << 5);
+
+    size_t value_size = 0;
+
+    if (n < 24) {
+        header |= (uint8_t)n;
+        n20_stream_prepend(s, &header, /*src_len=*/1);
+        return;
+    } else if (n < 0x100) {
+        header |= 24;
+        value_size = 1;
+    } else if (n < 0x10000) {
+        header |= 25;
+        value_size = 2;
+    } else if (n < 0x100000000) {
+        header |= 26;
+        value_size = 4;
+    } else {
+        header |= 27;
+        value_size = 8;
+    }
+
+    for (size_t i = 0; i < value_size; i++) {
+        uint8_t byte = (uint8_t)(n >> (i * 8));
+        n20_stream_prepend(s, &byte, /*src_len=*/1);
+    }
+
+    n20_stream_prepend(s, &header, /*src_len=*/1);
+}
+
+void n20_cbor_write_null(n20_stream_t *const s) {
+    n20_cbor_write_header(s, n20_cbor_type_simple_float_e, 22);
+}
+
+void n20_cbor_write_bool(n20_stream_t *const s, bool const b) {
+    n20_cbor_write_header(s, n20_cbor_type_simple_float_e, b ? 21 : 20);
+}
+
+void n20_cbor_write_tag(n20_stream_t *const s, uint64_t const tag) {
+    n20_cbor_write_header(s, n20_cbor_type_tag_e, tag);
+}
+
+void n20_cbor_write_uint(n20_stream_t *const s, uint64_t const n) {
+    n20_cbor_write_header(s, n20_cbor_type_uint_e, n);
+}
+
+void n20_cbor_write_int(n20_stream_t *const s, int64_t const n) {
+    if (n >= 0) {
+        n20_cbor_write_uint(s, (uint64_t)n);
+    } else {
+        n20_cbor_write_header(s, n20_cbor_type_nint_e, (uint64_t)(-n - 1));
+    }
+}
+
+void n20_cbor_write_byte_string(n20_stream_t *const s, n20_slice_t const bytes) {
+    if (bytes.size > 0 && bytes.buffer == NULL) {
+        n20_cbor_write_null(s);
+        return;
+    }
+
+    n20_stream_prepend(s, bytes.buffer, bytes.size);
+    n20_cbor_write_header(s, n20_cbor_type_bytes_e, bytes.size);
+}
+
+void n20_cbor_write_text_string(n20_stream_t *const s, n20_string_slice_t const text) {
+    if (text.size > 0 && text.buffer == NULL) {
+        n20_cbor_write_null(s);
+        return;
+    }
+
+    n20_stream_prepend(s, (uint8_t const *)text.buffer, text.size);
+    n20_cbor_write_header(s, n20_cbor_type_string_e, text.size);
+}
+
+void n20_cbor_write_array_header(n20_stream_t *const s, size_t const len) {
+    n20_cbor_write_header(s, n20_cbor_type_array_e, len);
+}
+
+void n20_cbor_write_map_header(n20_stream_t *const s, size_t const len) {
+    n20_cbor_write_header(s, n20_cbor_type_map_e, len);
+}
---
+++ src/core/test/cbor.cpp
@@ -0,0 +1,305 @@
+/*
+ * Copyright 2025 Aurora Operations, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <nat20/cbor.h>
+#include <nat20/stream.h>
+
+#include <cstdint>
+#include <tuple>
+#include <variant>
+#include <vector>
+
+class CborIntegerTestFixture
+    : public testing::TestWithParam<
+          std::tuple<std::variant<uint64_t, int64_t>, std::vector<uint8_t>>> {};
+
+INSTANTIATE_TEST_CASE_P(
+    CborIntegerTestInstance,
+    CborIntegerTestFixture,
+    testing::Values(
+        /* CBOR encoding encoding size boundary conditions. */
+        std::tuple(UINT64_C(0), std::vector<uint8_t>{0x00}),
+        std::tuple(UINT64_C(1), std::vector<uint8_t>{0x01}),
+        std::tuple(UINT64_C(23), std::vector<uint8_t>{0x17}),
+        std::tuple(UINT64_C(24), std::vector<uint8_t>{0x18, 0x18}),
+        std::tuple(UINT64_C(255), std::vector<uint8_t>{0x18, 0xff}),
+        std::tuple(UINT64_C(256), std::vector<uint8_t>{0x19, 0x01, 0x00}),
+        std::tuple(UINT64_C(0xffff), std::vector<uint8_t>{0x19, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x10000), std::vector<uint8_t>{0x1a, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffff), std::vector<uint8_t>{0x1a, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x100000000),
+                   std::vector<uint8_t>{0x1b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffffffffffff),
+                   std::vector<uint8_t>{0x1b, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}),
+        /* Repeat the same constants as above but force using the
+         * the indirection through n20_cbor_write_int. */
+        std::tuple(INT64_C(0), std::vector<uint8_t>{0x00}),
+        std::tuple(INT64_C(1), std::vector<uint8_t>{0x01}),
+        std::tuple(INT64_C(23), std::vector<uint8_t>{0x17}),
+        std::tuple(INT64_C(24), std::vector<uint8_t>{0x18, 0x18}),
+        std::tuple(INT64_C(255), std::vector<uint8_t>{0x18, 0xff}),
+        std::tuple(INT64_C(256), std::vector<uint8_t>{0x19, 0x01, 0x00}),
+        std::tuple(INT64_C(0xffff), std::vector<uint8_t>{0x19, 0xff, 0xff}),
+        std::tuple(INT64_C(0x10000), std::vector<uint8_t>{0x1a, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(INT64_C(0xffffffff), std::vector<uint8_t>{0x1a, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(INT64_C(0x100000000),
+                   std::vector<uint8_t>{0x1b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        std::tuple(INT64_MAX,
+                   std::vector<uint8_t>{0x1b, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(INT64_C(-1), std::vector<uint8_t>{0x20}),
+        std::tuple(INT64_C(-24), std::vector<uint8_t>{0x37}),
+        std::tuple(INT64_C(-25), std::vector<uint8_t>{0x38, 0x18}),
+        std::tuple(INT64_C(-256), std::vector<uint8_t>{0x38, 0xff}),
+        std::tuple(INT64_C(-257), std::vector<uint8_t>{0x39, 0x01, 0x00}),
+        std::tuple(INT64_C(-65536), std::vector<uint8_t>{0x39, 0xff, 0xff}),
+        std::tuple(INT64_C(-65537), std::vector<uint8_t>{0x3a, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(INT64_C(-4294967296), std::vector<uint8_t>{0x3a, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(INT64_C(-4294967297),
+                   std::vector<uint8_t>{0x3b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        /* This is not the lowest integer that can be represented with
+         * CBOR major type 1, but it is the lowest that can be represented
+         * using 64 bits 2s-complement. And thus the limit of the
+         * integer encoding functions as of now. */
+        std::tuple(INT64_MIN,
+                   std::vector<uint8_t>{0x3b, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}),
+
+        /* Known OpenDICE label values. */
+        std::tuple(INT64_C(-4670545), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x50}),
+        std::tuple(INT64_C(-4670546), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x51}),
+        std::tuple(INT64_C(-4670547), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x52}),
+        std::tuple(INT64_C(-4670548), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x53}),
+        std::tuple(INT64_C(-4670549), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x54}),
+        std::tuple(INT64_C(-4670550), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x55}),
+        std::tuple(INT64_C(-4670551), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x56}),
+        std::tuple(INT64_C(-4670552), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x57}),
+        std::tuple(INT64_C(-4670553), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x58}),
+        std::tuple(INT64_C(-4670554), std::vector<uint8_t>{0x3a, 0x00, 0x47, 0x44, 0x59})));
+
+TEST_P(CborIntegerTestFixture, CborIntegerTest) {
+    auto [integer, encoding] = GetParam();
+
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    if (std::holds_alternative<uint64_t>(integer)) {
+        n20_cbor_write_uint(&s, std::get<uint64_t>(integer));
+    } else {
+        n20_cbor_write_int(&s, std::get<int64_t>(integer));
+    }
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+    ASSERT_EQ(got_encoding, encoding);
+}
+
+TEST(CborTests, CborWriteNullTest) {
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_null(&s);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+
+    ASSERT_EQ(bytes_written, 1);
+    ASSERT_EQ(got_encoding, std::vector<uint8_t>{0xf6});
+}
+
+TEST(CborTests, CborWriteBoolTest) {
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_bool(&s, true);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+
+    ASSERT_EQ(bytes_written, 1);
+    ASSERT_EQ(got_encoding, std::vector<uint8_t>{0xf5});
+
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_bool(&s, false);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    bytes_written = n20_stream_byte_count(&s);
+    got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+
+    ASSERT_EQ(bytes_written, 1);
+    ASSERT_EQ(got_encoding, std::vector<uint8_t>{0xf4});
+}
+
+class CborTagTestFixture
+    : public testing::TestWithParam<std::tuple<uint64_t, std::vector<uint8_t>>> {};
+
+INSTANTIATE_TEST_CASE_P(
+    CborTagTestInstance,
+    CborTagTestFixture,
+    testing::Values(
+        /* CBOR encoding encoding size boundary conditions. */
+        std::tuple(UINT64_C(0), std::vector<uint8_t>{0xc0}),
+        std::tuple(UINT64_C(1), std::vector<uint8_t>{0xc1}),
+        std::tuple(UINT64_C(23), std::vector<uint8_t>{0xd7}),
+        std::tuple(UINT64_C(24), std::vector<uint8_t>{0xd8, 0x18}),
+        std::tuple(UINT64_C(255), std::vector<uint8_t>{0xd8, 0xff}),
+        std::tuple(UINT64_C(256), std::vector<uint8_t>{0xd9, 0x01, 0x00}),
+        std::tuple(UINT64_C(0xffff), std::vector<uint8_t>{0xd9, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x10000), std::vector<uint8_t>{0xda, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffff), std::vector<uint8_t>{0xda, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x100000000),
+                   std::vector<uint8_t>{0xdb, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffffffffffff),
+                   std::vector<uint8_t>{0xdb, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})));
+
+TEST_P(CborTagTestFixture, CborTagTest) {
+    auto [integer, encoding] = GetParam();
+
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_tag(&s, integer);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+    ASSERT_EQ(got_encoding, encoding);
+}
+
+TEST(CborTests, CborWriteByteStringTest) {
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    uint8_t bytes[] = {0x01, 0x02, 0x03, 0x04};
+    n20_cbor_write_byte_string(&s, {.size = sizeof(bytes), .buffer = bytes});
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+
+    auto want_encoding = std::vector<uint8_t>{0x44, 0x01, 0x02, 0x03, 0x04};
+
+    ASSERT_EQ(bytes_written, 5);
+    ASSERT_EQ(got_encoding, want_encoding);
+}
+
+TEST(CborTests, CborWriteStringTest) {
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_string_slice_t str = N20_STR_C("Hello");
+    n20_cbor_write_text_string(&s, str);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+
+    auto want_encoding = std::vector<uint8_t>{0x65, 0x48, 0x65, 0x6c, 0x6c, 0x6f};
+
+    ASSERT_EQ(bytes_written, 6);
+    ASSERT_EQ(got_encoding, want_encoding);
+}
+
+class CborArrayHeaderTestFixture
+    : public testing::TestWithParam<std::tuple<uint64_t, std::vector<uint8_t>>> {};
+
+INSTANTIATE_TEST_CASE_P(
+    CborArrayHeaderTestInstance,
+    CborArrayHeaderTestFixture,
+    testing::Values(
+        /* CBOR encoding encoding size boundary conditions. */
+        std::tuple(UINT64_C(0), std::vector<uint8_t>{0x80}),
+        std::tuple(UINT64_C(1), std::vector<uint8_t>{0x81}),
+        std::tuple(UINT64_C(23), std::vector<uint8_t>{0x97}),
+        std::tuple(UINT64_C(24), std::vector<uint8_t>{0x98, 0x18}),
+        std::tuple(UINT64_C(255), std::vector<uint8_t>{0x98, 0xff}),
+        std::tuple(UINT64_C(256), std::vector<uint8_t>{0x99, 0x01, 0x00}),
+        std::tuple(UINT64_C(0xffff), std::vector<uint8_t>{0x99, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x10000), std::vector<uint8_t>{0x9a, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffff), std::vector<uint8_t>{0x9a, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x100000000),
+                   std::vector<uint8_t>{0x9b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffffffffffff),
+                   std::vector<uint8_t>{0x9b, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})));
+
+TEST_P(CborArrayHeaderTestFixture, CborArrayHeaderTest) {
+    auto [integer, encoding] = GetParam();
+
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_array_header(&s, integer);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+    ASSERT_EQ(got_encoding, encoding);
+}
+
+class CborMapHeaderTestFixture
+    : public testing::TestWithParam<std::tuple<uint64_t, std::vector<uint8_t>>> {};
+
+INSTANTIATE_TEST_CASE_P(
+    CborMapHeaderTestInstance,
+    CborMapHeaderTestFixture,
+    testing::Values(
+        /* CBOR encoding encoding size boundary conditions. */
+        std::tuple(UINT64_C(0), std::vector<uint8_t>{0xa0}),
+        std::tuple(UINT64_C(1), std::vector<uint8_t>{0xa1}),
+        std::tuple(UINT64_C(23), std::vector<uint8_t>{0xb7}),
+        std::tuple(UINT64_C(24), std::vector<uint8_t>{0xb8, 0x18}),
+        std::tuple(UINT64_C(255), std::vector<uint8_t>{0xb8, 0xff}),
+        std::tuple(UINT64_C(256), std::vector<uint8_t>{0xb9, 0x01, 0x00}),
+        std::tuple(UINT64_C(0xffff), std::vector<uint8_t>{0xb9, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x10000), std::vector<uint8_t>{0xba, 0x00, 0x01, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffff), std::vector<uint8_t>{0xba, 0xff, 0xff, 0xff, 0xff}),
+        std::tuple(UINT64_C(0x100000000),
+                   std::vector<uint8_t>{0xbb, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}),
+        std::tuple(UINT64_C(0xffffffffffffffff),
+                   std::vector<uint8_t>{0xbb, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})));
+
+TEST_P(CborMapHeaderTestFixture, CborMapHeaderTest) {
+    auto [integer, encoding] = GetParam();
+
+    uint8_t buffer[20];
+
+    n20_stream_t s;
+    n20_stream_init(&s, &buffer[0], sizeof(buffer));
+
+    n20_cbor_write_map_header(&s, integer);
+
+    ASSERT_FALSE(n20_stream_has_buffer_overflow(&s));
+    size_t bytes_written = n20_stream_byte_count(&s);
+    auto got_encoding = std::vector(n20_stream_data(&s), n20_stream_data(&s) + bytes_written);
+    ASSERT_EQ(got_encoding, encoding);
+}
