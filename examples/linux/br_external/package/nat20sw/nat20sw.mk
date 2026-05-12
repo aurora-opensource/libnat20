f3f0549577416fc83d88c365453824d97b053130
Janis Danisevskis <jdanisevskis@aurora.tech>
2026-05-12 09:53:31 -0700

    Make nat20sw.mk consistent

--- examples/linux/br_external/package/nat20sw/nat20sw.mk
+++ examples/linux/br_external/package/nat20sw/nat20sw.mk
@@ -33,6 +33,10 @@
 # along with this program; if not, see
 # <https://www.gnu.org/licenses/>.
 
+# In CI NAT20SW_OVERRIDE_SRCDIR is set to the root of the repository,
+# so that the source under test is always the current branch.
+# Integrators who use this configuration should pin the version
+# to a specific commit or branch to avoid breakages when the main branch changes.
 NAT20SW_VERSION = origin/main
 NAT20SW_SITE = https://github.com/aurora-opensource/libnat20.git
 NAT20SW_SITE_METHOD = git
