b159fbb8b28aab50d8bfec4de9e1dd16296428d3
Janis Danisevskis <jdanisevskis@aurora.tech>
2026-05-15 08:50:38 -0700

    Revert "Add nat20cli command line tool for nat20device."
    This reverts commit 0612f25187bcb34eb608f2e3e90e1057e5c05fa4.

--- examples/linux/br_external/utils/envsetup.sh
+++ examples/linux/br_external/utils/envsetup.sh
@@ -46,7 +46,6 @@ fi
 
 source .env
 
-export NAT20CLI_OVERRIDE_SRCDIR="$LIBNAT20_ROOT"
 export NAT20CRYPTO_OVERRIDE_SRCDIR="$LIBNAT20_ROOT"
 export NAT20SW_OVERRIDE_SRCDIR="$LIBNAT20_ROOT"
 export NAT20DEVICE_OVERRIDE_SRCDIR="$LIBNAT20_ROOT"
@@ -74,7 +73,6 @@ function brrebuild() {
         echo "Available targets:"
         echo "  all          - Rebuild all components"
         echo "  linux        - Rebuild the linux kernel"
-        echo "  nat20cli     - Rebuild the Dice CLI"
         echo "  nat20crypto  - Rebuild the nat20crypto module"
         echo "  libnat20     - Rebuild the libnat20 library"
         echo "  nat20device  - Rebuild the nat20device module"
@@ -87,7 +85,7 @@ function brrebuild() {
 
     case "$1" in
         all)
-            ensure_popd make linux-rebuild nat20lib-rebuild nat20crypto-rebuild nat20device-rebuild nat20sw-rebuild libnat20-rebuild nat20cli-rebuild nat20test-rebuild all
+            ensure_popd make linux-rebuild nat20lib-rebuild nat20crypto-rebuild nat20device-rebuild nat20sw-rebuild libnat20-rebuild nat20test-rebuild all
             ;;
         *)
             ensure_popd make $1-rebuild all
